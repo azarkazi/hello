8da1f8df7a1f5ccff437055fd128b5965576e0ae
azarkazi <azarkazi1436@gmail.com>
2022-08-04 13:18:30 +0530

    Enhance the code: File Added Third Commit

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -16,11 +16,16 @@ public class HelloGit {
     public static void main(String[] args) {
         // TODO code application logic here
         showMe();
+        showBye();
     }
     
     public static void showMe(){
         System.out.println("Hello Git.");
         System.out.println("Good Day.");
+    }
+    
+    public static void showBye(){
+        System.out.println("Bye.");
     }
     
 }
